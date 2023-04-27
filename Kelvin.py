40e8e3f9d3c386edc4f0780e6ed6dc0598ecfc7c
Kelvin Akyea <73767888+khelvyn80@users.noreply.github.com>
2023-04-27 06:57:58 +0000

    Add files via upload

---
+++ Kelvin.py
@@ -0,0 +1,12 @@
+https://github.com/micropython/micropython
+https://github.com/adafruit/Adafruit_Learning_System_Guides
+https://github.com/micropython/micropython-lib
+https://github.com/neuralassembly/awesome-micropython
+https://github.com/randomnerd/esp32-micropython
+https://github.com/blynkkk/lib-python
+
+
+
+https://randomnerdtutorials.com/category/micropython/
+https://learn.adafruit.com/category/micropython
+https://micropython.org/
